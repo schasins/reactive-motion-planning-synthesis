0333df0ba324c27eb247673e460ca3ba8bfdb895
schasins <schasins@gmail.com>
2015-12-29 20:08:18 -0800

    Proper benchmark running.

--- src/generatedBenchmarks/runBenchmarks.sh
+++ src/generatedBenchmarks/runBenchmarks.sh
@@ -1,17 +1,18 @@
-for entry in `ls .`; do
+#!/bin/bash
 
+ITERATIONS=5
+
+for entry in `ls`; do
 	ARRAY=()
-	ITERATIONS=5
 
 	for ((i=0; i<$ITERATIONS;i++)); do
 	    for((j=0; j<2; j++)); do
-	        $PRETIME = date +%s%N
+	        PRETIME=$(date +%s%N)
 	        ~/research/sygus-comp14/solvers/enumerative/esolver-synth-lib/bin/opt/esolver-synthlib $entry
-	        $POSTTIME = date +%s%N
-	        $(ARRAY)+=($POSTTIME-$PRETIME)
+	        POSTTIME=$(date +%s%N)
+	        $(ARRAY)+= $POSTTIME-$PRETIME
 	    done
 	done
 
 	echo $entry $ARRAY
-
 done
