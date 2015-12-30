bb6728967441bfe6b824a50014cd3bf304becc19
schasins <schasins@gmail.com>
2015-12-29 21:48:00 -0800

    Changing structure slightly.

--- src/runBenchmarks.sh
+++ src/runBenchmarks.sh
@@ -4,12 +4,12 @@ function join { local IFS="$1"; shift; echo "$*"; }
 
 ITERATIONS=5
 
-for entry in `ls`; do
+for entry in `ls generatedBenchmarks`; do
 	ARRAY=()
 
 	for ((i=0; i<$ITERATIONS;i++)); do
 	        PRETIME=$(date +%s%N)
-	        eval ~/research/sygus-comp14/solvers/enumerative/esolver-synth-lib/bin/opt/esolver-synthlib "$entry" > solutions/"$entry"
+	        eval ~/research/sygus-comp14/solvers/enumerative/esolver-synth-lib/bin/opt/esolver-synthlib generatedBenchmarks/"$entry" > solutions/"$entry"
 	        POSTTIME=$(date +%s%N)
 	        ARRAY+=(`expr $POSTTIME - $PRETIME`)
 	done
