3ea353ebfcb7741df7d4cfe4ebc302331bfbf543
schasins <schasins@gmail.com>
2015-12-29 22:45:53 -0500

    No nested.

--- src/generatedBenchmarks/runBenchmarks.sh
+++ src/generatedBenchmarks/runBenchmarks.sh
@@ -3,19 +3,15 @@ for entry in `ls .`; do
 	ARRAY=()
 	ITERATIONS=5
 
-	for ((i=0; i<$ITERATIONS; i++)); do
-	    ARRAY+=(())
-	done
-
 	for ((i=0; i<$ITERATIONS;i++)); do
 	    for((j=0; j<2; j++)); do
 	        $PRETIME = date +%s%N
 	        ~/research/sygus-comp14/solvers/enumerative/esolver-synth-lib/bin/opt/esolver-synthlib $entry
 	        $POSTTIME = date +%s%N
-	        $(ARRAY[$(j)])+=($POSTTIME-$PRETIME)
+	        $(ARRAY)+=($POSTTIME-$PRETIME)
 	    done
 	done
 
 	echo $entry $ARRAY
-	
+
 done
